737969367b71d39e74554e234ed634f97e45f084
Shorh <Shorhina@gmail.com>
2019-01-13 13:57:02 +0500

    Упрощено решение

--- lesson3/task3.py
+++ lesson3/task3.py
@@ -2,23 +2,23 @@
 
 import random
 
-size = 10
-min_item = 0
-max_item = 100
-array = [random.randint(min_item, max_item) for _ in range(size)]
+SIZE = 10
+MIN_ITEM = 0
+MAX_ITEM = 100
+
+array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
 print(f'Исходный массив: \n{array}')
 
-min_elem = max_elem = array[0]
 min_i = max_i = 0
 
 for i in range(1, len(array)):
-    if min_elem > array[i]:
-        min_elem = array[i]
+    if array[min_i] > array[i]:
         min_i = i
-
-    if max_elem < array[i]:
-        max_elem = array[i]
+    elif array[max_i] < array[i]:
         max_i = i
+
+print(f'min = {array[min_i]} на {min_i} позиции')
+print(f'max = {array[max_i]} на {max_i} позиции')
 
 array[min_i], array[max_i] = array[max_i], array[min_i]
 print(f'Поменяли местами минимальный и максимальный элементы: \n{array}')

a7a15b97a8bc71d87f521a2bb59b79d938ccb075
Shorh <Shorhina@gmail.com>
2019-01-29 22:24:31 +0500

    Удалила старые комментарии

--- lesson6/task1.py
+++ lesson6/task1.py
@@ -3,25 +3,22 @@
 # Примечание: Для анализа возьмите любые 1-3 ваших программы или несколько вариантов кода для одной и той же задачи.
 # Результаты анализа вставьте в виде комментариев к коду. Также укажите в комментариях версию Python и разрядность
 # вашей ОС.
-#
-# !!!ВНИМАНИЕ!!! Решение добавлены в комменты для каждой функции и в выводы в конце
 
 
-# import cProfile
 import sys
 import inspect
 from memory_profiler import profile
 from collections import deque
 
 
-# для расчета времени выполнения алгоритма
 # N содержит в себе n-значное число в зависимости от IND
 N = []
 IND = [10, 100, 1000, 10000]
 for ind in IND:
     num = [1234567890] * (ind // 10)
     N.append(int("".join(map(str, num))))
-MEMORY = {}
+
+memory = {}
 
 
 def show_size(x):
@@ -42,21 +39,10 @@ def show_size(x):
 def reverse(n):
     """
     Рекурсия
-
-    100 loops, best of 3: 2.04 usec per loop      - 10 элементов
-    100 loops, best of 3: 44.5 usec per loop      - 100 элементов
-    100 loops, best of 3: 1.78 msec per loop      - 1 000 элементов
-    RuntimeError                                  - 10 000 элементов
-
-    10/1    0.000    0.000    0.000    0.000 task1.py:30(rev)      - 10 элементов
-    91/1    0.000    0.000    0.000    0.000 task1.py:30(rev)      - 100 элементов
-    901/1    0.003    0.000    0.003    0.003 task1.py:30(rev)     - 1 000 элементов
-    RecursionError                                                 - 10 000 элементов
-
     потребление памяти - 2 296 байт     - 10 элементов
     """
 
-    # для расчета времени выполнения алгоритма
+    # для расчета объема выделяемой памяти
     n_num = N[n]
 
     # для проверки работы функции тестом
@@ -66,16 +52,16 @@ def reverse(n):
     # @profile(precision=5)
     def rev(a, a_rev):
         for locs in locals().values():
-            MEMORY[inspect.stack()[-2][3]] += show_size(locs)
+            memory[inspect.stack()[-2][3]] += show_size(locs)
 
         if a == 0:
             return a_rev
         else:
             return rev(a // 10, a_rev * 10 + a % 10)
 
-    MEMORY[inspect.stack()[0][3]] = 0
+    memory[inspect.stack()[0][3]] = 0
     for locs in locals().values():
-        MEMORY[inspect.stack()[0][3]] += show_size(locs)
+        memory[inspect.stack()[0][3]] += show_size(locs)
 
     return rev(n_num, 0)
 
@@ -84,35 +70,24 @@ def reverse(n):
 def reverse_loop_1(n):
     """
     Цикл 1
-
-    100 loops, best of 3: 1.26 usec per loop      - 10 элементов
-    100 loops, best of 3: 38.7 usec per loop      - 100 элементов
-    100 loops, best of 3: 1.45 msec per loop      - 1 000 элементов
-    100 loops, best of 3: 117 msec per loop       - 10 000 элементов
-
-    1    0.000    0.000    0.000    0.000 task1.py:51(reverse_loop_1)      - 10 элементов
-    1    0.000    0.000    0.000    0.000 task1.py:51(reverse_loop_1)      - 100 элементов
-    1    0.001    0.001    0.001    0.001 task1.py:51(reverse_loop_1)      - 1 000 элементов
-    1    0.119    0.119    0.119    0.119 task1.py:51(reverse_loop_1)      - 10 000 элементов
-
     потребление памяти - 76 байт     - 10 элементов
     """
 
-    # для расчета времени выполнения алгоритма
+    # для расчета объема выделяемой памяти
     n_num = N[n]
 
     # для проверки работы функции тестом
     # n_num = [i + 1 for i in range(n)]
     # n_num = int("".join(map(str, n_num)))
 
-    MEMORY[inspect.stack()[0][3]] = 0
+    memory[inspect.stack()[0][3]] = 0
     n_rev = 0
     while n_num > 0:
         n_rev = n_rev * 10 + n_num % 10
         n_num = n_num // 10
 
     for locs in locals().values():
-        MEMORY[inspect.stack()[0][3]] += show_size(locs)
+        memory[inspect.stack()[0][3]] += show_size(locs)
 
     return n_rev
 
@@ -121,35 +96,24 @@ def reverse_loop_1(n):
 def reverse_loop_2(n):
     """
     Цикл 2
-
-    100 loops, best of 3: 1.86 usec per loop      - 10 элементов
-    100 loops, best of 3: 7.74 usec per loop      - 100 элементов
-    100 loops, best of 3: 117 usec per loop       - 1 000 элементов
-    100 loops, best of 3: 2.89 msec per loop      - 10 000 элементов
-
-    1    0.000    0.000    0.000    0.000 task1.py:79(reverse_loop_2)      - 10 элементов
-    1    0.000    0.000    0.000    0.000 task1.py:79(reverse_loop_2)      - 100 элементов
-    1    0.000    0.000    0.000    0.000 task1.py:79(reverse_loop_2)      - 1 000 элементов
-    1    0.003    0.003    0.003    0.003 task1.py:79(reverse_loop_2)      - 10 000 элементов
-
     потребление памяти - 192 байта     - 10 элементов
     """
 
-    # для расчета времени выполнения алгоритма
+    # для расчета объема выделяемой памяти
     n_num = N[n]
 
     # для проверки работы функции тестом
     # n_num = [i + 1 for i in range(n)]
     # n_num = int("".join(map(str, n_num)))
 
-    MEMORY[inspect.stack()[0][3]] = 0
+    memory[inspect.stack()[0][3]] = 0
     n_rev = ''
     n_num = str(n_num)
     for i in n_num:
         n_rev = i + n_rev
 
     for locs in locals().values():
-        MEMORY[inspect.stack()[0][3]] += show_size(locs)
+        memory[inspect.stack()[0][3]] += show_size(locs)
 
     return int(n_rev)
 
@@ -158,21 +122,10 @@ def reverse_loop_2(n):
 def reverse_slice(n):
     """
     Срезы
-
-    100 loops, best of 3: 0.811 usec per loop      - 10 элементов
-    100 loops, best of 3: 1.25 usec per loop       - 100 элементов
-    100 loops, best of 3: 18.3 usec per loop       - 1 000 элементов
-    100 loops, best of 3: 1.44 msec per loop       - 10 000 элементов
-
-    1    0.000    0.000    0.000    0.000 task1.py:107(reverse_slice)      - 10 элементов
-    1    0.000    0.000    0.000    0.000 task1.py:107(reverse_slice)      - 100 элементов
-    1    0.000    0.000    0.000    0.000 task1.py:107(reverse_slice)      - 1 000 элементов
-    1    0.002    0.002    0.002    0.002 task1.py:107(reverse_slice)      - 10 000 элементов
-
     потребление памяти - 115 байт   - 10 элементов
     """
 
-    # для расчета времени выполнения алгоритма
+    # для расчета объема выделяемой памяти
     n_num = N[n]
 
     # для проверки работы функции тестом
@@ -181,9 +134,9 @@ def reverse_slice(n):
 
     n_rev = str(n_num)[::-1]
 
-    MEMORY[inspect.stack()[0][3]] = 0
+    memory[inspect.stack()[0][3]] = 0
     for locs in locals().values():
-        MEMORY[inspect.stack()[0][3]] += show_size(locs)
+        memory[inspect.stack()[0][3]] += show_size(locs)
 
     return int(n_rev)
 
@@ -192,21 +145,10 @@ def reverse_slice(n):
 def reverse_deque(n):
     """
     deque
-
-    100 loops, best of 3: 2.49 usec per loop       - 10 элементов
-    100 loops, best of 3: 4.86 usec per loop       - 100 элементов
-    100 loops, best of 3: 44.3 usec per loop       - 1 000 элементов
-    100 loops, best of 3: 1.64 msec per loop       - 10 000 элементов
-
-    1    0.000    0.000    0.000    0.000 task1.py:134(reverse_deque)      - 10 элементов
-    1    0.000    0.000    0.000    0.000 task1.py:134(reverse_deque)      - 100 элементов
-    1    0.000    0.000    0.000    0.000 task1.py:134(reverse_deque)      - 1 000 элементов
-    1    0.002    0.002    0.002    0.002 task1.py:134(reverse_deque)      - 10 000 элементов
-
     потребление памяти - 1 188 байт   - 10 элементов
     """
 
-    # для расчета времени выполнения алгоритма
+    # для расчета объема выделяемой памяти
     n_num = N[n]
 
     # для проверки работы функции тестом
@@ -216,9 +158,9 @@ def reverse_deque(n):
     n_rev = deque(str(n_num))
     n_rev.reverse()
 
-    MEMORY[inspect.stack()[0][3]] = 0
+    memory[inspect.stack()[0][3]] = 0
     for locs in locals().values():
-        MEMORY[inspect.stack()[0][3]] += show_size(locs)
+        memory[inspect.stack()[0][3]] += show_size(locs)
 
     return int(''.join(n_rev))
 
@@ -240,21 +182,7 @@ def reverse_deque(n):
 # test_rev(reverse_deque)
 
 
-# cProfile.run('reverse(3)')
-# cProfile.run('reverse_loop_1(3)')
-# cProfile.run('reverse_loop_2(3)')
-# cProfile.run('reverse_slice(3)')
-# cProfile.run('reverse_deque(3)')
-
-
 # Выводы:
-# - рекурсия сдалась на 10 000 элементов
-# - Цикл 1 на 100 000 чуть не заставил ноут взлететь
-# - Все функции, кроме рекурсии, вызываются 1 раз
-# - Лучше всего использовать срезы: у встроенной фунции минимальное время выполнения
-# - Если не учитывать срезы и очереди, то оптимальный вариант обрабатывать число как строку и просто менять местами
-#   элементы
-#
 # - OS: macOS Mojave
 # - разрядность: 64 бита
 # - с точки зрения потребления памяти оптиальный вариант - деление на 10 в цикле
@@ -268,5 +196,5 @@ reverse_loop_2(0)
 reverse_slice(0)
 reverse_deque(0)
 
-for name in MEMORY:
-    print(f'Функция {name} потребляет {MEMORY[name]} байт памяти под переменные для разворота числа из 10 цифр')
+for name in memory:
+    print(f'Функция {name} потребляет {memory[name]} байт памяти под переменные для разворота числа из 10 цифр')
